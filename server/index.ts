776def21a5290a8920b22ac0babac813e930e9cf
Rohan Godha <git@rohangodha.com>
2024-09-28 15:54:52 -0400

    fix auth token expiration

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import { generateState, OAuth2RequestError, Spotify } from "arctic";
 import { spotify } from "./auth";
 import { getCookie, setCookie } from "hono/cookie";
 import { jwt, sign, type JwtVariables } from "hono/jwt";
-import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { use } from "hono/jsx";
 
 await connectToMongo();
@@ -27,45 +27,64 @@ app.use(
 app.get("/authed/playlist", async (c) => {
   const payload = c.get("jwtPayload");
   console.log(payload);
-  const user  = await Users.findOne({_id: payload.id})
-  if (!user){
-    return c.body(null, 400)
+  const user = await Users.findOne({ _id: payload.id });
+  if (!user) {
+    return c.body(null, 400);
   }
-  const API = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID!,{access_token: user.spotify_token, refresh_token: user.spotify_refresh_token, expires_in: user.spotify_token_iat.getTime(), token_type: "Bearer"} );
+
+  const API = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID!, {
+    access_token: user.spotify_token,
+    refresh_token: user.spotify_refresh_token,
+    expires_in: user.spotify_token_iat.getTime(),
+    token_type: "Bearer",
+  });
+
   const playlists = await API.playlists.getUsersPlaylists(user.spotify_id);
   const playlistsItems = playlists.items;
   const playlist_fields: any[] = [];
   const songLinks = [];
-  const searchYoutube = async (search: string) =>{
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(search)}&type=video&key=${process.env.GOOGLE_KEY}`)
+  const searchYoutube = async (search: string) => {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(search)}&type=video&key=${process.env.GOOGLE_KEY}`,
+    );
     const data = await response.json();
-    const youtubeId= data.items[0].snippet.id.videoId;
+    const youtubeId = data.items[0].snippet.id.videoId;
     return youtubeId;
-    }
-  const downloadVideo = async (url: string) =>{
-    const download = await fetch("https://cobalt.tools/api",{ method:"POST",
-      headers: {Accept: "application/json",
-        "Content-Type": "application/json"},
-      body: JSON.stringify({url:url,audioFormat: "wav", downloadMode: "audio"
-      })
-    })
+  };
+
+  const downloadVideo = async (url: string) => {
+    const download = await fetch("https://cobalt.tools/api", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: url,
+        audioFormat: "wav",
+        downloadMode: "audio",
+      }),
+    });
     return download;
-  }
-  for (const playlist of playlistsItems){
+  };
+
+  for (const playlist of playlistsItems) {
     const playlistId = playlist.id;
     const trackList = await API.playlists.getPlaylistItems(playlistId);
     playlist_fields.push({
       name: playlist.name,
       cover: playlist.images,
-      songs: playlist.tracks.items.map(track => track.track.name)
-    })
-    for (let song of trackList.items){
-      const search = `${song.track.name} by ${song.track.artists.map(artist => artist.name).join(', ')}`;
+      songs: playlist.tracks.items.map((track) => track.track.name),
+    });
+
+    for (let song of trackList.items) {
+      const search = `${song.track.name} by ${song.track.artists.map((artist) => artist.name).join(", ")}`;
       const youtubeUrl = `youtube.com/watch?v=${await searchYoutube(search)}`;
       const download = (await downloadVideo(youtubeUrl)).url;
       songLinks.push(download);
     }
   }
+
   return c.json({ authed: true });
 });
 app.get("/auth/login", async (c) => {
@@ -116,7 +135,7 @@ app.get("/auth/callback", async (c) => {
       const token = await sign(
         {
           id: existingUser._id,
-          exp: Math.floor(Date.now() * 1000) + 60 * 60 * 24, // Token expires in 24hrs
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365, // Token expires in 1y
         },
         process.env.JWT_SECRET!,
       );
@@ -138,7 +157,7 @@ app.get("/auth/callback", async (c) => {
     const token = await sign(
       {
         id: user._id,
-        exp: Math.floor(Date.now() * 1000) + 60 * 60 * 24, // Token expires in 24hrs
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365, // Token expires in 1y
       },
       process.env.JWT_SECRET!,
     );
