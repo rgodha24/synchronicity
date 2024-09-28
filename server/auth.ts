cf26d71fee46f9fdc1f6d99cc502d81d5db02a04
calvin-k347 <clark.calvink@gmail.com>
2024-09-28 15:33:59 -0400

    added playlist route to download yt urls of playlist songs

--- server/auth.ts
+++ server/auth.ts
@@ -3,5 +3,5 @@ import { Spotify } from "arctic";
 export const spotify = new Spotify(
   process.env.SPOTIFY_CLIENT_ID!,
   process.env.SPOTIFY_CLIENT_SECRET!,
-  "http://10.0.2.2:3000/auth/callback",
+  "http://localhost:3000/auth/callback",
 );
